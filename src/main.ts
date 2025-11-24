9c6a91e70988d02a39d2fdbfba48ced17633ad58
daniu006 <dpabad@sudamericano.edu.ec>
2025-11-24 11:23:08 -0500

    despliegue

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
   app.enableCors({
     origin: '*', // Cambia esto a tu dominio del frontend en producción
     credentials: true,
   });
 
-  // Validation pipe
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,7 +19,7 @@ async function bootstrap() {
     }),
   );
 
-  // Puerto dinámico para Railway
+  
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   
